98ee7a2bff90a1daa8ae10a527cdea6d787d3523
uowol <alllhvhla@gmail.com>
2021-09-03 09:45:58 +0900

    fixed error

--- data/1.getSummonersByTier.py
+++ data/1.getSummonersByTier.py
@@ -7,13 +7,13 @@ from API import *
 # PAGE        = "1"
 
 ### get league data
-url = GET("kr", FILE_NAME, True)
+url = GET("kr", "lol/league/v4/entries/{queue}/{tier}/{division}?page={page}".format(tier=TIER, division=DIVISION, queue=QUEUE, page="1"), True)
 res = requests.get(url)
 
 if res.status_code == 200:
     json_data = json.loads(res.text)
     print("read successfully : {length}".format(length=len(json_data)))
-    with open("data_summoner_{tier}_{division}_{queue}.json".format(tier=TIER, division=DIVISION, queue=QUEUE), 'w') as outfile:
+    with open(FILE_NAME, 'w') as outfile:
         json.dump(json_data, outfile, indent=4)
 else:
     print("error")
